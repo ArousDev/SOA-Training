919ab73baa2e165223101f29080257a6f6b60dc0
ArousDev <arasti.arous@gmail.com>
2026-02-11 01:13:04 +0100

    ue crud RestAPIs

--- Gestion_UE_Etudiant/src/main/java/webservices/UniteEnseignementRestApi.java
+++ Gestion_UE_Etudiant/src/main/java/webservices/UniteEnseignementRestApi.java
@@ -6,6 +6,8 @@ import metiers.UniteEnseignementBusiness;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.Map;
 
 @Path("/ue")
 public class UniteEnseignementRestApi {
@@ -22,13 +24,24 @@ public class UniteEnseignementRestApi {
     }
 
     @GET
-    @Path("/get/{id}")
+    @Path("/get/id/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getById(@PathParam("id") int id) {
-        return Response
-                .status(200)
-                .entity(this.helper.getUEByCode(id))
-                .build();
+        UniteEnseignement ue = this.helper.getUEByCode(id);
+        if(ue != null)
+            return Response
+                    .status(Response.Status.FOUND)
+                    .entity(Map.of(
+                            "message", "ue with id = " + id + " exists.",
+                            "data", ue
+                    ))
+                    .build();
+
+        else
+            return Response
+                    .status(Response.Status.NOT_FOUND)
+                    .entity("no ue with id = " + id + " in List.")
+                    .build();
     }
 
     @GET
@@ -37,13 +50,85 @@ public class UniteEnseignementRestApi {
     @Produces(MediaType.APPLICATION_JSON)
     public Response add(UniteEnseignement ue) {
         ue = new UniteEnseignement(6, "add", "Mme Maroua Douiri", 6, 1);
+        if(this.helper.addUniteEnseignement(ue))
+            return Response
+                    .status(Response.Status.CREATED)
+                    .entity(Map.of(
+                            "message", "ue added successfully.",
+                            "data", ue
+                    ))
+                    .build();
 
-        return Response
-                .status(200)
-                .entity(this.helper.addUniteEnseignement(ue))
-                .build();
+        else
+            return Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity("Could not add ue")
+                    .build();
     }
 
+    @GET
+    @Path("/get/sem/{nb}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getBySemester(@PathParam("nb") int nb) {
+        List<UniteEnseignement> list = this.helper.getUEBySemestre(nb);
+        if(!list.isEmpty())
+            return Response
+                    .status(Response.Status.FOUND)
+                    .entity(Map.of(
+                            "message", list.size() + " ues with semester = " + nb + " were found.",
+                            "data", list
+                    ))
+                    .build();
 
+        else
+            return Response
+                    .status(Response.Status.NOT_FOUND)
+                    .entity("no ue with semester = " + nb + " in List.")
+                    .build();
+    }
+
+    @GET
+    @Path("/delete/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response delete(@PathParam("id") int id) {
+        UniteEnseignement ue = helper.getUEByCode(id);
+        if(helper.deleteUniteEnseignement(id))
+            return Response
+                    .status(Response.Status.OK)
+                    .entity(Map.of(
+                            "message", "ue successfully deleted.",
+                            "data", ue
+                    ))
+                    .build();
+
+        else
+            return Response
+                    .status(Response.Status.NOT_FOUND)
+                    .entity("no ue with code = " + id + " in List.")
+                    .build();
+    }
+
+    @GET
+    @Path("/update/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response update(@PathParam("id") int id) {
+        UniteEnseignement ue = new UniteEnseignement(id, "update", "Mme Maroua Douiri", 6, 1);
+        if(helper.updateUniteEnseignement(id,ue))
+            return Response
+                    .status(Response.Status.OK)
+                    .entity(Map.of(
+                            "message", "ue successfully updated",
+                            "data", ue
+                    ))
+                    .build();
+
+        else
+            return Response
+                    .status(Response.Status.NOT_FOUND)
+                    .entity("no ue with code = " + id + " in List.")
+                    .build();
+    }
 
 }
